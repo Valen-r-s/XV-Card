d692a9ceb0cbfdc94d622588d96f98fb7a3ee203
Valentina Restrepo <valentina.re.san@gmail.com>
2025-05-21 08:59:37 -0500

    counter fixed

--- Counter.js
+++ Counter.js
@@ -5,6 +5,20 @@ function updateCountdown() {
   const now = new Date().getTime();
   const distance = deadline - now;
 
+  if (distance <= 0) {
+    clearInterval(timer); // Detener el contador
+    countdown.innerHTML = `
+      <div class="time-box"><span>00</span><small>Días</small></div>
+      <div class="separator">:</div>
+      <div class="time-box"><span>00</span><small>Horas</small></div>
+      <div class="separator">:</div>
+      <div class="time-box"><span>00</span><small>Minutos</small></div>
+      <div class="separator">:</div>
+      <div class="time-box"><span>00</span><small>Segundos</small></div>
+    `;
+    return;
+  }
+
   const days = Math.floor(distance / (1000 * 60 * 60 * 24));
   const hours = Math.floor(
     (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
@@ -13,27 +27,27 @@ function updateCountdown() {
   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   countdown.innerHTML = `
-  <div class="time-box"><span>${String(days).padStart(
-    2,
-    "0"
-  )}</span><small>Días</small></div>
-  <div class="separator">:</div>
-  <div class="time-box"><span>${String(hours).padStart(
-    2,
-    "0"
-  )}</span><small>Horas</small></div>
-  <div class="separator">:</div>
-  <div class="time-box"><span>${String(minutes).padStart(
-    2,
-    "0"
-  )}</span><small>Minutos</small></div>
-  <div class="separator">:</div>
-  <div class="time-box"><span>${String(seconds).padStart(
-    2,
-    "0"
-  )}</span><small>Segundos</small></div>
-`;
+    <div class="time-box"><span>${String(days).padStart(
+      2,
+      "0"
+    )}</span><small>Días</small></div>
+    <div class="separator">:</div>
+    <div class="time-box"><span>${String(hours).padStart(
+      2,
+      "0"
+    )}</span><small>Horas</small></div>
+    <div class="separator">:</div>
+    <div class="time-box"><span>${String(minutes).padStart(
+      2,
+      "0"
+    )}</span><small>Minutos</small></div>
+    <div class="separator">:</div>
+    <div class="time-box"><span>${String(seconds).padStart(
+      2,
+      "0"
+    )}</span><small>Segundos</small></div>
+  `;
 }
 
 updateCountdown();
-setInterval(updateCountdown, 1000);
+const timer = setInterval(updateCountdown, 1000);
